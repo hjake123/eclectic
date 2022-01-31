c77f04cce58cc80c7510b9447a3812120c023ec0
Hyperlynx <hjake123@gmail.com>
2022-01-30 21:17:22 -0600

    Completed the Phantom Quilt item and improved the "weighed down" effect.

--- src/main/java/com/hyperlynx/eclectic/blocks/PhantomQuiltBlock.java
+++ src/main/java/com/hyperlynx/eclectic/blocks/PhantomQuiltBlock.java
@@ -28,9 +28,9 @@ import java.util.Random;
 public class PhantomQuiltBlock extends Block {
     public static final BooleanProperty WEIGHED = BooleanProperty.create("weighed");
 
-    protected static final VoxelShape SHAPE = Block.box(1, 2, 1, 15, 3, 15);
-    protected static final VoxelShape SHAPE_LOW = Block.box(1, 1, 1, 15, 2, 15);
-    protected static final VoxelShape TOUCH_SHAPE = Block.box(1, 2, 1, 15, 4, 15);
+    protected static final VoxelShape SHAPE = Block.box(1, 3, 1, 15, 4, 15);
+    protected static final VoxelShape SHAPE_LOW = Block.box(1, 2, 1, 15, 3, 15);
+    protected static final VoxelShape TOUCH_SHAPE = Block.box(1, 3, 1, 15, 5, 15);
 
     public PhantomQuiltBlock(Properties prop) {
         super(prop);
@@ -43,47 +43,32 @@ public class PhantomQuiltBlock extends Block {
 
     @Override
     public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        return state.getValue(WEIGHED) ? SHAPE_LOW : SHAPE;
+        return SHAPE;
     }
 
     @Override
     public void fallOn(@NotNull Level level, @NotNull BlockState state, @NotNull BlockPos pos, @NotNull Entity p_153365_, float p_153366_) {
         // Don't cause fall damage! ;)
-        // state.setValue(WEIGHED, shouldBeWeighed(state, level, pos));
     }
 
     @Override
     public void entityInside(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Entity entity) {
         if(!level.isClientSide() && !state.getValue(WEIGHED)) {
-            boolean last = state.getValue(WEIGHED);
             level.setBlockAndUpdate(pos, state.setValue(WEIGHED, shouldBeWeighed(state, level, pos)));
-            if (last != level.getBlockState(pos).getValue(WEIGHED)) {
-                EclecticMod.LOGGER.info("Changed state due to entity presence!");
-            }
             level.scheduleTick(new BlockPos(pos), this, 10); // Schedule a tick to test if we should remain weighed down.
         }
     }
 
     @Override
     public void tick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull Random rand) {
-        boolean last = state.getValue(WEIGHED);
         level.setBlockAndUpdate(pos, state.setValue(WEIGHED, shouldBeWeighed(state, level, pos)));
         if(state.getValue(WEIGHED)){
             level.scheduleTick(pos, this, 10);
         }
-        if(last != level.getBlockState(pos).getValue(WEIGHED)){
-            EclecticMod.LOGGER.info("Changed state due to tick!");
-        }
     }
 
     private boolean shouldBeWeighed(@NotNull BlockState state, Level level, @NotNull BlockPos pos){
-        Vec3 corner = Vec3.atBottomCenterOf(pos);
-        List<Entity> ents = level.getEntities(null, TOUCH_SHAPE.bounds().move(pos));
-        EclecticMod.LOGGER.info("Testing if should be weighed! I think: " + (ents.size() > 0));
-        return ents.size() > 0;
+        return level.getEntities(null, TOUCH_SHAPE.bounds().move(pos)).size() > 0;
     }
-
-        // TODO: Change block state when stepped on, sink by 1 pixel.
-
 
 }
--- src/main/java/com/hyperlynx/eclectic/items/PhantomQuiltItem.java
+++ src/main/java/com/hyperlynx/eclectic/items/PhantomQuiltItem.java
@@ -1,13 +1,30 @@
 package com.hyperlynx.eclectic.items;
 
 import com.hyperlynx.eclectic.Registration;
+import com.hyperlynx.eclectic.blocks.PhantomQuiltBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
 
 public class PhantomQuiltItem extends BlockItem {
     public PhantomQuiltItem(Properties pProperties) {
         super(Registration.PHANTOM_QUILT.get(), pProperties);
     }
 
-    // TODO: Catch the player as they fall.
+    @Override
+    public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int slot, boolean selected){
+        int ACTIVATE_HEIGHT = 8; // Number of blocks to fall before activating. Configurable?
+        if(entity.fallDistance > ACTIVATE_HEIGHT && !entity.isInvulnerableTo(DamageSource.FALL)){
+            BlockPos underfoot = entity.blockPosition().below();
+            if(level.isEmptyBlock(underfoot)) {
+                level.setBlock(underfoot, Registration.PHANTOM_QUILT.get().defaultBlockState(), 2);
+                stack.setCount(0);
+            }
+        }
+    }
 }
